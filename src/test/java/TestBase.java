370cbee8aece4b8256c07408c58c031a1e28a9b3
Marina Horuzhaya <marinakh192@gmail.com>
2023-09-14 22:52:36 +0300

    fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -38,6 +38,9 @@ public class TestBase {
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
 
     }
 }
