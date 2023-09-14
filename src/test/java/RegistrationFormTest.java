cccd8af58995baf25f3e3bc02c4a1a667de75e27
Marina Horuzhaya <marinakh192@gmail.com>
2023-09-14 21:34:42 +0300

    fix

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -1,6 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +16,9 @@ import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormTest  extends TestBase{
 
+    @DisplayName("заполнение формы с помощью steps")
+    @Owner("HoruzhayaMarina")
+    @Severity(SeverityLevel.BLOCKER)
     @Tag("simple")
     @Test
     void fillFormTest() {
