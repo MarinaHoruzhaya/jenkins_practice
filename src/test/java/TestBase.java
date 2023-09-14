395ed4d1e37894d89aee402c5e6379ca94ced8ed
Marina Horuzhaya <marinakh192@gmail.com>
2023-09-14 21:02:38 +0300

    added steps

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        //    Configuration.pageLoadStrategy = "eager";
+    //    Configuration.browser = "chrome";
+        Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC",true,
+                "enableVideo", true
+
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+}
